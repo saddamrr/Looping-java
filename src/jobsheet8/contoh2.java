b4470260f3c446d1183ca99197090150829cc3c8
S A D D A M <S A D D A M@Windows10>
2018-08-03 12:47:43 +0700

    Bilangan 1-10

--- src/jobsheet8/contoh2.java
+++ src/jobsheet8/contoh2.java
@@ -1,19 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jobsheet8;
-import java.util.Scanner;
+
 public class contoh2 {
     public static void main(String[] args)
     {
         int x;
-        
+    
         for(x=1; x <= 10; x++)
         {
             System.out.print(x + " ");
-        }
+}
         
         System.out.println();
         
